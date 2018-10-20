ab49dfd89b87711889c826908a5e9ac26f90cc13
Fahad Haidari <fahad.haidari@gmail.com>
2018-10-19 20:42:44 -0600

    Push code

---
+++ index.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const width = canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let angle = 0;
+    let factor = 0;
+
+    context.translate(width / 2, 50);
+    context.scale(1, -1);
+
+    setInterval(() => {
+        angle += 0.08;
+        const x = Math.cos(angle) * angle;
+        const y = Math.sin(angle) * angle;
+        factor += 50;
+
+        if(factor % 1000 == 0) {
+          context.fillStyle = "#" + Math.random().toString(16).substr(2, 6);
+        }
+        context.fillRect(x, y - angle, 10 * Math.cos(angle), 5 * Math.sin(factor));
+    }, 1);
+}
